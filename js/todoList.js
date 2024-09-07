a7983e93a3bddfb1ee844003dcf77560eb97acd3
ageesea <abarquezalvin10@gmail.com>
2024-09-07 15:48:13 +0800

    change for loop to forEach

--- js/todoList.js
+++ js/todoList.js
@@ -10,20 +10,19 @@ renderTodoList ();
 function renderTodoList (){
   let todolistHTML = '';
 
-  for(let i =0; i< todoList.length; i++){
-    const todoObject = todoList[i];
-
+  todoList.forEach(function(todoObject, index){
     const {name, dueDate}= todoObject;
     const html = `
       <div>${name}</div>
       <div>${dueDate}</div>
       <button onclick="
-        todoList.splice(${i}, 1);
+        todoList.splice(${index}, 1);
         renderTodoList();
       " class="delete-todo-button">Delete</button> 
     `;
     todolistHTML+=html;
-  }
+
+  });
   
   document.querySelector('.js-todo-list').innerHTML =todolistHTML;
 }
